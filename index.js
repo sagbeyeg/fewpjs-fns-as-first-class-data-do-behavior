a366a1169e8f4b952132898d96780377fb882719
sagbeyeg <agbeyegbesisan@gmail.com>
2020-10-28 13:02:50 -0400

    Completed

--- index.js
+++ index.js
@@ -10,4 +10,25 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(str) {
+  const hour = parseInt(str.split(':'));
+  console.log(hour);
+
+  if (hour < 12) return "Good Morning"
+  if (hour > 17) return "Good Evening"
+  return "Good Afternoon"  
+}
+
+// console.log(greet("11:24"))
+// console.log(greet("12:24"))
+// console.log(greet("19:24"))
+
 /* Write your implementation of displayMessage() */
+function displayMessage(str) {
+ document.getElementById("greeting").innerText = str 
+}
+// let hi = "Test"
+// console.log(displayMessage(hi))
+
+// console.log(document.getElementById("greeting").innerText)
+
